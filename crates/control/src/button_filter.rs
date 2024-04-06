11439dcdc6f18b450af03433a759e351a6f84b8e
Vivituhh <vivienne.schwabe@tuhh.de>
2024-04-06 16:43:35 +0200

    fake OverArmPose with first head button touch

--- crates/control/src/button_filter.rs
+++ crates/control/src/button_filter.rs
@@ -2,10 +2,16 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use color_eyre::Result;
 use context_attribute::context;
+use coordinate_systems::{Field, Ground};
 use filtering::tap_detector::TapDetector;
 use framework::MainOutput;
+use hardware::NetworkInterface;
+use linear_algebra::Isometry2;
 use serde::{Deserialize, Serialize};
-use types::{buttons::Buttons, cycle_time::CycleTime, sensor_data::SensorData};
+use spl_network_messages::{HulkMessage, PlayerNumber};
+use types::{
+    buttons::Buttons, cycle_time::CycleTime, messages::OutgoingMessage, sensor_data::SensorData,
+};
 
 #[derive(Deserialize, Serialize)]
 pub struct ButtonFilter {
@@ -14,6 +20,8 @@ pub struct ButtonFilter {
     last_head_buttons_touched: bool,
     calibration_buttons_touched: SystemTime,
     last_calibration_buttons_touched: bool,
+    first_head_button_touched: SystemTime,
+    last_first_head_button_touched: bool,
 }
 
 #[context]
@@ -26,6 +34,10 @@ pub struct CycleContext {
 
     calibration_buttons_timeout: Parameter<Duration, "button_filter.calibration_buttons_timeout">,
     head_buttons_timeout: Parameter<Duration, "button_filter.head_buttons_timeout">,
+
+    time_to_reach_kick_position: CyclerState<Duration, "time_to_reach_kick_position">,
+
+    hardware: HardwareInterface,
 }
 
 #[context]
@@ -42,10 +54,12 @@ impl ButtonFilter {
             last_head_buttons_touched: false,
             calibration_buttons_touched: UNIX_EPOCH,
             last_calibration_buttons_touched: false,
+            first_head_button_touched: UNIX_EPOCH,
+            last_first_head_button_touched: false,
         })
     }
 
-    pub fn cycle(&mut self, context: CycleContext) -> Result<MainOutputs> {
+    pub fn cycle(&mut self, context: CycleContext<impl NetworkInterface>) -> Result<MainOutputs> {
         let head_buttons_timeout = *context.head_buttons_timeout;
         let calibration_buttons_timeout = *context.calibration_buttons_timeout;
         let touch_sensors = &context.sensor_data.touch_sensors;
@@ -70,6 +84,40 @@ impl ButtonFilter {
                 .duration_since(self.head_buttons_touched)
                 .unwrap()
                 >= head_buttons_timeout;
+
+                
+
+        let first_head_button_touched = touch_sensors.head_front && !touch_sensors.head_middle && !touch_sensors.head_rear;
+
+        let first_head_button_touched_initially =
+            first_head_button_touched && !self.last_first_head_button_touched;
+
+        if first_head_button_touched_initially {
+            self.first_head_button_touched = context.cycle_time.start_time;
+        }
+        self.last_first_head_button_touched = first_head_button_touched;
+
+        let debounced_first_head_button_touched = first_head_button_touched
+            && context
+                .cycle_time
+                .start_time
+                .duration_since(self.first_head_button_touched)
+                .unwrap()
+                >= head_buttons_timeout;
+
+        if debounced_first_head_button_touched {
+            context
+                .hardware
+                .write_to_network(OutgoingMessage::Spl(HulkMessage {
+                    player_number: PlayerNumber::Seven,
+                    fallen: false,
+                    pose: Isometry2::<Ground, Field>::identity().as_pose(),
+                    over_arms_pose_detected: true,
+                    ball_position: None,
+                    time_to_reach_kick_position: Some(*context.time_to_reach_kick_position),
+                }))?;
+            println!("Initial -> Ready")
+        }
 
         let calibration_buttons_touched = touch_sensors.chest_button && touch_sensors.head_front;
 
--- crates/spl_network/src/endpoint.rs
+++ crates/spl_network/src/endpoint.rs
@@ -70,6 +70,7 @@ impl Endpoint {
                     let (received_bytes, _address) = result.map_err(Error::ReadError)?;
                     match bincode::deserialize(&spl_buffer[0..received_bytes]) {
                         Ok(parsed_message) => {
+                            dbg!(&parsed_message);
                             break Ok(IncomingMessage::Spl(parsed_message));
                         }
                         Err(error) => {
@@ -91,6 +92,7 @@ impl Endpoint {
             }
             OutgoingMessage::Spl(message) => match bincode::serialize(&message) {
                 Ok(message) => {
+                    dbg!(&message);
                     if let Err(error) = self
                         .spl_socket
                         .send_to(
